61e8ccd0ba8e25cb7e564c9912bc755161a55eb7
Jim Jenkins <jimjenkins5@gmail.com>
2017-10-30 20:43:52 -0400

    Fix SetupUbuntu.sh

--- SetupUbuntu.sh
+++ SetupUbuntu.sh
@@ -1,10 +1,12 @@
+sudo apt-get update && sudo apt-get upgrade -y
+
 # miscellaneous packages for stuff and things
-sudo apt-get install build-essential libevent-dev libncurses5-dev
+sudo apt-get -qy install build-essential libevent-dev libncurses5-dev
 
 # tmux 2.6
 cd ~
 wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
-tar -xfz ~/tmux-2.6.tar.gz
+tar -xzf tmux-2.6.tar.gz
 cd ~/tmux-2.6/
 ./configure && make
 sudo make install
@@ -12,8 +14,8 @@ cd ~ && rm -rf ~/tmux-2.6
 
 # nvm install
 curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-nvm use lts/*
+nvm install lts/* && nvm use lts/*
 
 # mysql
 export DEBIAN_FRONTEND=noninteractive
-sudo -E apt-get -q -y install mysql-server
+sudo -E apt-get -qy install mysql-server
