c86bf853e241308b9879ec34beceb4def3452779
Jim Jenkins <jimjenkins5@gmail.com>
2018-12-31 15:09:13 -0500

    Fix global gitignore and auto set the config value

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -29,3 +29,6 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+# ensure that the global gitignore setting is correct (it's an absolute value)
+git config --global core.excludesfile ~/.gitignore_global
