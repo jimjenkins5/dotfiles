59ea304cba9eb075e3f08dc8d3ac05fc29db39b5
Jim Jenkins <jimjenkins5@gmail.com>
2020-05-30 11:26:42 -0400

    fix: make awsume work with pyenv

--- bash/aws.sh
+++ bash/aws.sh
@@ -1,6 +1,15 @@
 # For awsume (https://awsu.me)
-alias awsume=". awsume"
-alias AWS=". awsume"
+
+AWSUME_COMMAND="awsume"
+# If pyenv is installed, we need to alias awsume command to the full path of the awsume
+# file before re-aliasing it as `. awsume`. Otherwise, pyenv will error when looking for
+# the script using '. awsume' as the name.
+# See https://github.com/trek10inc/awsume/issues/49
+if [ $(command -v pyenv) ]; then
+   AWSUME_COMMAND=$(pyenv which awsume)
+fi
+alias awsume=". $AWSUME_COMMAND"
+alias AWS=". $AWSUME_COMMAND"
 
 _awsume() {
     local cur prev opts
