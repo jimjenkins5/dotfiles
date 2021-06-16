989343164975c5d0663c83d5a6d625b4db68b514
Jim Jenkins <jimjenkins5@gmail.com>
2021-06-16 09:32:05 -0400

    fix: remmove old pyenv code

--- frameworks/setup.sh
+++ frameworks/setup.sh
@@ -16,6 +16,5 @@ if [ -d "${HOME}/.pyenv" ]; then
    export PATH="${PYENV_ROOT}/bin:${PATH}"
    if [ $(command -v pyenv) ]; then
       eval "$(pyenv init -)"
-      eval "$(pyenv virtualenv-init -)"
    fi
 fi
