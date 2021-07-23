82595125f318bf14099b5c02d13927a13e659083
Jim Jenkins <jimjenkins5@gmail.com>
2021-07-23 12:43:13 -0400

    fix: correct pyenv init command

--- frameworks/setup.sh
+++ frameworks/setup.sh
@@ -15,7 +15,6 @@ if [ -d "${HOME}/.pyenv" ]; then
    export PYENV_ROOT="${HOME}/.pyenv"
    export PATH="${PYENV_ROOT}/bin:${PATH}"
    if [ $(command -v pyenv) ]; then
-      eval "$(pyenv init --path)"
       eval "$(pyenv init -)"
    fi
 fi
