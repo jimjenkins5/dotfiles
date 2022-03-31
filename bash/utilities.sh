b7f2a1dd10622b86898ee6faaf1856b5f98a6a82
Jim Jenkins <jimjenkins5@gmail.com>
2022-03-31 14:59:46 -0400

    feat: add watch_file utility

--- bash/utilities.sh
+++ bash/utilities.sh
@@ -1,3 +1,13 @@
+# brew install jq
+# decodeJWT <JWT>
 function decodeJWT {
   echo $1 | sed -e 's/.*\.\(.*\)\..*/\1/' | base64 -d | jq '.';
+}
+
+# brew install kqwait
+# watch_file <file|dir> <command>
+function watch_file() {
+  file=$(echo $1);
+  shift;
+  while kqwait $file; do $@; done
 }
