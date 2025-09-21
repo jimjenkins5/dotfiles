2a6e0f0e8f8f4a99e72b1c5c37d8d4b2e37b5483
Jim Jenkins <jimjenkins5@gmail.com>
2025-09-21 15:50:55 +0200

    fix: only setup homebrew if it exists

--- frameworks/homebrew.sh
+++ frameworks/homebrew.sh
@@ -1,7 +1,9 @@
 # Set up HOMEBREW_PREFIX (/opt/homebrew for M1, /usr/local for everything else)
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if [ -d "/opt/homebrew/" ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 
-export PATH="${HOMEBREW_PREFIX}/bin:${HOMEBREW_PREFIX}/sbin${PATH+:$PATH}";
-export MANPATH="${HOMEBREW_PREFIX}/share/man${MANPATH+:$MANPATH}:";
-export INFOPATH="${HOMEBREW_PREFIX}/share/info:${INFOPATH:-}";
-export PATH="${HOMEBREW_PREFIX}/bin:${PATH}"
+  export PATH="${HOMEBREW_PREFIX}/bin:${HOMEBREW_PREFIX}/sbin${PATH+:$PATH}";
+  export MANPATH="${HOMEBREW_PREFIX}/share/man${MANPATH+:$MANPATH}:";
+  export INFOPATH="${HOMEBREW_PREFIX}/share/info:${INFOPATH:-}";
+  export PATH="${HOMEBREW_PREFIX}/bin:${PATH}"
+fi
